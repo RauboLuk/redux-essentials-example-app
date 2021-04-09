ae18cb81d433f966023919fe9a057891f0fd81a2
RauboLuk <rauboluk@gmail.com>
2021-04-09 23:28:54 +0200

    P5, Loading State for Requests done

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,10 +1,10 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit"
-import { sub } from 'date-fns'
 
-const initialState = [
-  { id: '1', date: sub(new Date(), { minutes: 10 }).toISOString(), title: 'First Post!', content: 'Hello!', user: 0, reactions: {thumbsUp: 5, hooray: 24, heart: 1, rocket: 7, eyes: 20} },
-  { id: '2', date: sub(new Date(), { minutes: 5 }).toISOString(), title: 'Second Post', content: 'More text', user: 1, reactions: {thumbsUp: 0, hooray: 24, heart: 11, rocket: 34, eyes: 1} }
-]
+const initialState = {
+  posts: [],
+  status: 'idle',
+  error: null
+}
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -12,14 +12,14 @@ const postsSlice = createSlice({
   reducers: {
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.find(post => post.id === postId)
+      const existingPost = state.posts.find(post => post.id === postId)
       if (existingPost) {
         existingPost.reactions[reaction]++
       }
     },
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload)
+        state.posts.push(action.payload)
       },
       prepare(title, content, userId) {
         return {
@@ -36,7 +36,7 @@ const postsSlice = createSlice({
     },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
-      const existingPost = state.find(post => post.id === id)
+      const existingPost = state.posts.find(post => post.id === id)
       if (existingPost) {
         existingPost.title = title
         existingPost.content = content
@@ -48,3 +48,9 @@ const postsSlice = createSlice({
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
+
+export const selectAllPosts = state => state.posts.posts
+
+export const selectPostById = (state, postId) =>
+  state.posts.posts.find(post => post.id === postId)
+  
